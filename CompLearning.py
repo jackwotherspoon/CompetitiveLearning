48d6aafe2b20cfe99adc9450a03a392202562ec8
Jack Wotherspoon <jackwotherspoon@hotmail.com>
2019-03-06 20:21:33 -0500

    Add euclidean distance and kohonen guess.

--- CompLearning.py
+++ CompLearning.py
@@ -3,6 +3,7 @@
 
 from random import random
 from csv import reader
+import numpy as np
 from math import exp
 
 # Load a CSV file
@@ -20,3 +21,19 @@ filename='dataset_noclass.csv'
 dataset=load_csv(filename)
 dataset.pop(0)  #remove row with column titles
 print(dataset)
+
+def euclid_dist(weight_vec,input_vec):
+    dist = np.sqrt(np.sum(np.square(np.subtract(input_vec, weight_vec))))
+    return dist
+
+def kohonen_guess(self, input_arr):
+    node_dists = []
+    for key in self.layer_weights.keys():
+        weights_mat = self.layer_weights[key]
+        num_rows = weights_mat.shape[0]
+        for row in range(num_rows):
+            weight_row = weights_mat[row,]
+            dist = euclid_dist(weight_row, input_arr)
+            node_dists.append(dist)
+    winner = np.argmin(node_dists)  # returns the index of the smallest distance
+    return winner
